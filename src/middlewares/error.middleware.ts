f07a801957f07e0279b68f9360ac1b0cabb8568d
Wahyu Pratama <wahyupraunsri@gmail.com>
2024-10-22 22:16:24 +0700

    remove middleware

--- src/middlewares/error.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-function errorMiddleware (err: Error, req: Request, res: Response, next: NextFunction ){
-    res.status(500).send({
-        message: err.message,
-    });
-}
-
-export default errorMiddleware;
